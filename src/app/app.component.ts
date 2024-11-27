5d31468c19909ce191894d9a4752564320556d18
Thiago da Silva Rangel <slipknothiago@gmail.com>
2024-11-27 08:15:10 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+import { DUMMY_USERS } from '../data/dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'essentials';
+  title = 'essentials'
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)
+  }
+
+  onSelectUser(id : string) {
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return `users/${this.user.avatar}`
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
